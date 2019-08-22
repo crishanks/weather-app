1f9163a815b119be2d253867d0b0d0c747e8b157
github username <crishanks12@gmail.com>
2019-08-21 21:56:52 -0600

    geocoding for lat and long with mapbox api

--- app.js
+++ app.js
@@ -5,3 +5,12 @@ const url = 'https://api.darksky.net/forecast/5ea6789990c966cb5445fdba4e2497ae/3
 request({ url: url, json: true }, (error, response) => {
   console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`)
 })
+
+const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY3Jpc2NvZGVzIiwiYSI6ImNqem01MDc0NzEyZTczYms2c3ZxN3VidGMifQ.UtAZQMh9gJfMWdJjSnn4eA"
+
+request({ url: geocodeURL, json: true}, (error, response) => {
+  
+  const lat = response.body.features[0].center[1];
+  const long = response.body.features[0].center[0];
+  console.log(lat, long)
+})
