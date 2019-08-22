99d7109e9c8c47c5c6a9d073434af42852e0db40
github username <crishanks12@gmail.com>
2019-08-21 23:12:30 -0600

    async call to geocode api. separated geocode into utils directory

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 const request = require('request')
+const geocode = require('./utils/geocode')
 
-const url = 'https://api.darksky.net/forecast/5ea6789990c966cb5445fdba4e2497ae/37.8267,-122.4233?units=us&lang=es'
+// const url = 'https://api.darksky.net/forecast/5ea6789990c966cb5445fdba4e2497ae/37.8267,-122.4233?units=us&lang=es'
 
-request({ url: url, json: true }, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to weather service!')
-  } else if (response.body.error) {
-    console.log('Unable to find location!')
-  } else {
-    console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`)
-  }
-})
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log('Unable to connect to weather service!')
+//   } else if (response.body.error) {
+//     console.log('Unable to find location!')
+//   } else {
+//     console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`)
+//   }
+// })
 
-const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoiY3Jpc2NvZGVzIiwiYSI6ImNqem01MDc0NzEyZTczYms2c3ZxN3VidGMifQ.UtAZQMh9gJfMWdJjSnn4eA"
-
-request({ url: geocodeURL, json: true}, (error, response) => {
-  if (error) {
-    console.log('Unable to connect to location services!')
-  } else if (response.body.features.length === 0) {
-    console.log('Unable to find location. Try another search.')
-  } else {
-    const lat = response.body.features[0].center[1];
-    const long = response.body.features[0].center[0];
-    console.log(lat, long)
-  }
+geocode('Utah', (error, data) => {
+  console.log('Error', error);
+  console.log('Data', data)
 })
