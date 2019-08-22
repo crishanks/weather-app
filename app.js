2c18986b8eec54d9036fa5e1eba90b2e654c92bd
github username <crishanks12@gmail.com>
2019-08-21 18:21:37 -0600

    unit and lang added to request url. updated request output;

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const request = require('request')
 
-const url = 'https://api.darksky.net/forecast/5ea6789990c966cb5445fdba4e2497ae/37.8267,-122.4233'
+const url = 'https://api.darksky.net/forecast/5ea6789990c966cb5445fdba4e2497ae/37.8267,-122.4233?units=us&lang=es'
 
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body)
-  console.log(data.currently)
+request({ url: url, json: true }, (error, response) => {
+  console.log(`${response.body.daily.data[0].summary} It is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain.`)
 })
